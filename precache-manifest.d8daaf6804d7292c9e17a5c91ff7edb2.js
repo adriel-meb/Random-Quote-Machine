0de2fa4fc3eac14d403afd4d35f46fd6f963d113
Adriel <adrix92@live.fr>
2020-05-24 23:45:44 +0200

    Updates

---
+++ precache-manifest.d8daaf6804d7292c9e17a5c91ff7edb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5188f2273afea659f83aef09bf2a261",
+    "url": "/Random-Quotes-Machine/index.html"
+  },
+  {
+    "revision": "475cb6a04fbd86b724ea",
+    "url": "/Random-Quotes-Machine/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9a13caea2f55df21d6de",
+    "url": "/Random-Quotes-Machine/static/css/main.fc820c1f.chunk.css"
+  },
+  {
+    "revision": "475cb6a04fbd86b724ea",
+    "url": "/Random-Quotes-Machine/static/js/2.e69d682a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Random-Quotes-Machine/static/js/2.e69d682a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a13caea2f55df21d6de",
+    "url": "/Random-Quotes-Machine/static/js/main.1db45023.chunk.js"
+  },
+  {
+    "revision": "8266d9bcbd95e0014e1d",
+    "url": "/Random-Quotes-Machine/static/js/runtime-main.9f764d66.js"
+  }
+]);
